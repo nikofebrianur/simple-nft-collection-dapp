3b71e556b0b93558efc69581fdfed48715bb12a0
Niko Febrianur <nikofebrianur@gmail.com>
2023-01-04 10:13:50 +0800

    fix image path

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,6 +4,7 @@ import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+import Image from 'next/image';
 
 export default function Home() {
 	const [walletConnected, setWalletConnected] = useState(false);
@@ -150,7 +151,7 @@ export default function Home() {
 					{renderButton()}
 				</div>
 				<div>
-					<img className={styles.image} src="../images/crypto-devs.svg" />
+					<img className={styles.image} src="/crypto-devs.svg" />
 				</div>
 
 				<footer className={styles.footer}>
